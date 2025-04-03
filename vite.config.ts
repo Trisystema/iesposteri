0721dffca7d091bce7ea01a31e41b5f048b70a78
granddragoman <speovic@gmail.com>
2025-04-03 09:15:47 +0200

    Config fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,18 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
+import {fileURLToPath} from 'url'
+import {dirname, resolve} from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   base: "/iesposteri/",
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src")
+      "@": resolve(__dirname, "./src")
     }
   }
 })
